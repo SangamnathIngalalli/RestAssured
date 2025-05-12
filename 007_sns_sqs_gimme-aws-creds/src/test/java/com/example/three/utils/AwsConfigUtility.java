095a71b51aca85770c2b3e1bbd9a2896934dcf69
Sangamnath Ingalalli <sangamnath.professional@gmail.com>
2025-05-12 22:32:23 +0530

    property file

--- 007_sns_sqs_gimme-aws-creds/src/test/java/com/example/three/utils/AwsConfigUtility.java
+++ 007_sns_sqs_gimme-aws-creds/src/test/java/com/example/three/utils/AwsConfigUtility.java
@@ -2,20 +2,44 @@ package com.example.three.utils;
 
 import com.amazonaws.regions.Regions;
 
-/**
- * Utility class for AWS configuration values
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+/**
+ * Utility class for AWS configuration values, loaded from a properties file.
+ */
 public class AwsConfigUtility {
 
-    // AWS resource identifiers
-    private static final String SNS_TOPIC_ARN = "YOUR_SNS_TOPIC_ARN";
-    private static final String SQS_QUEUE_URL = "YOUR_SQS_QUEUE_URL";
-    private static final Regions REGION = Regions.US_EAST_1;
-    
-    // JSON file path
-    private static final String MESSAGE_JSON_PATH = "src/test/resources/three/message_payload.json";
-    
+    private static final Properties properties = new Properties();
+    private static final String SNS_TOPIC_ARN;
+    private static final String SQS_QUEUE_URL;
+    private static final Regions REGION;
+    private static final String MESSAGE_JSON_PATH;
+
+    static {
+        try (InputStream input = AwsConfigUtility.class.getClassLoader().getResourceAsStream("aws_config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find aws_config.properties");
+                // Set default values or throw an error if the file is critical
+                SNS_TOPIC_ARN = "YOUR_SNS_TOPIC_ARN_DEFAULT"; 
+                SQS_QUEUE_URL = "YOUR_SQS_QUEUE_URL_DEFAULT";
+                REGION = Regions.US_EAST_1; // Default region
+                MESSAGE_JSON_PATH = "src/test/resources/three/message_payload.json";
+            } else {
+                properties.load(input);
+                SNS_TOPIC_ARN = properties.getProperty("sns.topic.arn");
+                SQS_QUEUE_URL = properties.getProperty("sqs.queue.url");
+                REGION = Regions.fromName(properties.getProperty("aws.region"));
+                MESSAGE_JSON_PATH = properties.getProperty("message.json.path");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            // Handle exception, perhaps by throwing a runtime exception or using defaults
+            throw new RuntimeException("Failed to load aws_config.properties", ex);
+        }
+    }
+
     /**
      * Gets the SNS topic ARN
      * 
@@ -24,7 +48,7 @@ public class AwsConfigUtility {
     public static String getSnsTopicArn() {
         return SNS_TOPIC_ARN;
     }
-    
+
     /**
      * Gets the SQS queue URL
      * 
@@ -33,7 +57,7 @@ public class AwsConfigUtility {
     public static String getSqsQueueUrl() {
         return SQS_QUEUE_URL;
     }
-    
+
     /**
      * Gets the AWS region
      * 
@@ -42,7 +66,7 @@ public class AwsConfigUtility {
     public static Regions getRegion() {
         return REGION;
     }
-    
+
     /**
      * Gets the path to the message JSON file
      * 
